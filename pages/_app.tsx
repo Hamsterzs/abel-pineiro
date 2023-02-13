35df58bca4be8cf1c71cd0805a3563adc04404d7
Hamsterzs <abel090713@gmail.com>
2023-02-12 18:07:38 -0600

    Landing page Desktop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Abel, Prompt, Dancing_Script } from "@next/font/google";
+
+const abel = Abel({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-abel",
+});
+
+const prompt = Prompt({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-prompt",
+});
+
+const dancing_script = Dancing_Script({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-dancing-script",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main
+      className={`${abel.variable} ${prompt.variable} ${dancing_script.variable}`}
+    >
+      <Component {...pageProps} />;
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,56 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    container: { center: true },
+    extend: {
+      keyframes: {
+        "spin-reverse": {
+          to: { transform: "rotate(-360deg)" },
+        },
+      },
+      animation: {
+        orbit: "spin 180s linear infinite",
+        "orbit-reverse": "spin-reverse 180s linear infinite",
+        "orbit-slow": "spin 200s linear infinite",
+        "orbit-slow-reverse": "spin-reverse 200s linear infinite",
+      },
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+
+        "3xl": "1920px",
+        // => @media (min-width: 1920px) { ... }
+
+        "4xl": "2560px",
+        // => @media (min-width: 2560px) { ... }
+      },
+      fontFamily: {
+        abel: ["var(--font-abel)", ...fontFamily.sans],
+        prompt: ["var(--font-prompt)", ...fontFamily.sans],
+        dancingScript: ["var(--font-dancing-script)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
