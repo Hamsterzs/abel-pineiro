ec52c1cf638d9eba3868cac7a249f16127792a59
Hamsterzs <abel090713@gmail.com>
2023-04-11 12:45:42 -0500

    IDK see what happens

---
+++ src/app/music/home/page.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import MusicPage from "../../../components/MusicPage";
+import { GetValidatorIn } from "../../../server/Music/schemas/music/getMusic";
+import { getMusic } from "../../../server/Music";
+import getLastSongs from "../../../server/Spotify/getLastSongs";
+
+// 3 hours in seconds
+export const revalidate = 10800;
+
+const getMusicProps = async (urlQuery: {
+  type: string;
+  sortBy: string;
+  order: string;
+}) => {
+  const query: GetValidatorIn = {
+    type: urlQuery.type,
+    query: {
+      sortBy: urlQuery.sortBy,
+      order: urlQuery.order,
+    },
+  };
+
+  return await getMusic({ type: urlQuery.type, query });
+};
+
+const Page = async ({ searchParams }: { searchParams: any }) => {
+  const { type, sortBy, order } = searchParams;
+
+  const music = await getMusicProps({
+    order: order,
+    sortBy: sortBy,
+    type: type,
+  });
+
+  const myLastSongs = await getLastSongs();
+
+  return <MusicPage music={music} myLastSongs={myLastSongs} />;
+};
+
+export default Page;
