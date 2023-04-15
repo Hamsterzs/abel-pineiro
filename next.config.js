ee711f92edc732eb7ed2341a118d8f6b23adbbdb
Hamsterzs <abel090713@gmail.com>
2023-04-15 00:46:54 -0500

    I feel the bleed

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ const { withSuperjson } = require("next-superjson");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+  // i18n: {
+  //   locales: ["en"],
+  //   defaultLocale: "en",
+  // },
   experimental: { appDir: true, serverComponentsExternalPackages: ["mysql2"] },
 };
 
