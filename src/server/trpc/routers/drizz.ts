87fa976a1e1cd3991d89fe01d54d85a08b06c192
Hamsterzs <abel090713@gmail.com>
2023-07-29 19:57:55 -0500

    Remove trash

--- src/server/trpc/routers/drizz.ts
+++
@@ -1,9 +0,0 @@
-import { allTransactions, seedTransactions } from "../../../drizzle/db";
-import { procedure, router } from "../trpc";
-
-export const drizzRouter = router({
-  seed: procedure.mutation(async () => {
-    return await seedTransactions();
-  }),
-  get: procedure.query(() => allTransactions()),
-});
