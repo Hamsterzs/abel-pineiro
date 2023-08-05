d5fbcdf27e20a8fb6a52aff236ead36877fe2120
Hamsterzs <abel090713@gmail.com>
2023-08-05 15:53:53 -0500

    Move edge route

--- src/app/edge-music/page.tsx
+++ src/app/edge-music/page.tsx
@@ -1,15 +1,13 @@
 import React, { Suspense } from "react";
-import MusicPage from "../../../components/MusicPage";
-import { db } from "../../../drizzle/db";
-import LastSongs, { LastSongsLoader } from "../../../components/LastSongs";
-import getLastSongs from "../../../server/Spotify/getLastSongs";
-import getMusic from "../../../server/getMusic";
+import MusicPage from "../../components/MusicPage";
+import LastSongs, { LastSongsLoader } from "../../components/LastSongs";
+import getLastSongs from "../../server/Spotify/getLastSongs";
+import getMusic from "../../server/getMusic";
 
 export const runtime = "edge";
 
 const Page = async () => {
   const LastSongsPromise = getLastSongs();
-
   const music = await getMusic();
 
   return (
