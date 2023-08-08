da8434e2451919cc8523f8bf0963a435d2cca513
Hamsterzs <abel090713@gmail.com>
2023-08-07 23:45:26 -0500

    Move back

--- src/app/drizz-music/page.tsx
+++ src/app/drizz-music/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import MusicPage from "../../components/MusicPageDrizz";
 import getMusic from "../../server/getMusic";
 
+export const revalidate = false;
+export const dynamic = "force-static";
+
 const Page = async () => {
   const music = await getMusic();
 
