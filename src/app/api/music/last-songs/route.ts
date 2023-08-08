973aab528a6ab913974c5cdb112531ad2e52c1d8
Hamsterzs <abel090713@gmail.com>
2023-08-07 23:41:15 -0500

    Remove revalidate

--- src/app/api/music/last-songs/route.ts
+++ src/app/api/music/last-songs/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 import getLastSongs from "../../../../utils/getLastSongs";
 
-export const revalidate = 60;
-
 export async function GET() {
   const lastSongs = await getLastSongs();
 
