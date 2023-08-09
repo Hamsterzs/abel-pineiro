b3b9a3bde9184adf95d04a7df0e1948a77db02f0
Hamsterzs <abel090713@gmail.com>
2023-08-08 22:03:55 -0500

    export dynamic

--- src/app/drizz-music/page.tsx
+++ src/app/drizz-music/page.tsx
@@ -1,20 +1,57 @@
 import React from "react";
 import MusicPage from "../../components/MusicPageDrizz";
-import getMusic from "../../server/getMusic";
-import { z } from "zod";
 import { unstable_cache } from "next/cache";
 import getLastSongs from "../../utils/getLastSongs";
+import { db } from "../../drizzle/db";
+import { album, artist, image, song } from "../../drizzle/schema";
+import { eq } from "drizzle-orm";
+import { MusicData } from "../../server/Music/types/MusicData";
 
-export const fetchCache = "default-no-store";
+export const dynamic = "force-static";
+
+const getMusic = async () => {
+  // return [
+  //   {
+  //     id: "1",
+  //     image: "/black-parade.jpg",
+  //     rating: 5,
+  //     title: "Song " + Math.random(),
+  //     subTitle: "Artist " + Math.random(),
+  //   },
+  // ];
+  const songs = await db
+    .select({
+      id: song.id,
+      name: song.title,
+      artist: artist.name,
+      image: image.url,
+      rating: song.rating,
+    })
+    .from(song)
+    .leftJoin(artist, eq(song.artistId, artist.id))
+    .leftJoin(album, eq(song.albumId, album.id))
+    .leftJoin(image, eq(album.imageId, image.id));
+  const musicData: MusicData[] = songs.map(
+    (song) =>
+      ({
+        id: song.id,
+        image: song.image,
+        rating: song.rating,
+        title: song.name,
+        subTitle: song.artist,
+      } as MusicData)
+  );
+
+  return musicData;
+};
 
 const Page = async () => {
-  const getMusicCached = unstable_cache(getMusic, ["music-key"], {
+  const music = await unstable_cache(getMusic, ["music-key"], {
     tags: ["music-tag"],
     revalidate: false,
-  });
+  })();
 
   const lastSongs = getLastSongs();
-  const music = await getMusicCached();
 
   return <MusicPage music={music} />;
 };
