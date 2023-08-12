222ff195d844ac598651074e2972dfa92b085bf6
Hamsterzs <abel090713@gmail.com>
2023-08-11 20:34:42 -0500

    Just fetch idk

--- src/app/drizz-music/page.tsx
+++ src/app/drizz-music/page.tsx
@@ -1,46 +1,28 @@
 import React from "react";
 import MusicPage from "../../components/MusicPageDrizz";
 import { unstable_cache } from "next/cache";
-import getLastSongs from "../../utils/getLastSongs";
 import { db } from "../../drizzle/db";
 import { album, artist, image, song } from "../../drizzle/schema";
 import { eq } from "drizzle-orm";
 import { MusicData } from "../../server/Music/types/MusicData";
 
 const getMusic = async () => {
-  const songs = await db
-    .select({
-      id: song.id,
-      name: song.title,
-      artist: artist.name,
-      image: image.url,
-      rating: song.rating,
-    })
-    .from(song)
-    .leftJoin(artist, eq(song.artistId, artist.id))
-    .leftJoin(album, eq(song.albumId, album.id))
-    .leftJoin(image, eq(album.imageId, image.id));
-  const musicData: MusicData[] = songs.map(
-    (song) =>
-      ({
-        id: song.id,
-        image: song.image,
-        rating: song.rating,
-        title: song.name,
-        subTitle: song.artist,
-      } as MusicData)
-  );
+	const baseUrl = process.env.BASE_URL
+	const res = await fetch(baseUrl + '/api/music/getMusic', {next: {revalidate: false, tags: ['MusicData']}})
+	const data = (await res.json()) as MusicData[]
 
-  return musicData;
+	return data
 };
 
 const Page = async () => {
+	/*
   const music = await unstable_cache(getMusic, ["music-key"], {
     tags: ["music-tag"],
     revalidate: false,
-  })();
+	})();
+	*/
 
-  const lastSongs = getLastSongs();
+	const music = await getMusic()
 
   return <MusicPage music={music} />;
 };
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,18 +1,12 @@
 // db.ts
-// import { drizzle } from "drizzle-orm/planetscale-serverless";
-import mysql from "mysql2/promise";
-import { drizzle } from "drizzle-orm/mysql2";
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { connect } from "@planetscale/database";
 
-// import { connect } from "@planetscale/database";
-
-// // create the connection
-// const connection = connect({
-//   host: process.env["DATABASE_HOST"],
-//   username: process.env["DATABASE_USERNAME"],
-//   password: process.env["DATABASE_PASSWORD"],
-// });
-
-const connection = await (async () =>
-  mysql.createConnection(process.env["DATABASE_URL"] as string))();
+// create the connection
+const connection = connect({
+  host: process.env["DATABASE_HOST"],
+  username: process.env["DATABASE_USERNAME"],
+  password: process.env["DATABASE_PASSWORD"],
+});
 
 export const db = drizzle(connection);
