0456b343ef63614bfa79a1713caa92e39ca4185f
Hamsterzs <abel090713@gmail.com>
2023-08-12 13:48:32 -0500

    Pls work, For me...

--- src/app/api/music/getMusic/route.ts
+++ src/app/api/music/getMusic/route.ts
@@ -6,6 +6,7 @@ import { MusicData } from "../../../../server/Music/types/MusicData";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 export async function GET() {
   const songs = await db
--- src/app/api/music/last-songs/route.ts
+++ src/app/api/music/last-songs/route.ts
@@ -3,6 +3,7 @@ import getLastSongs from "../../../../utils/getLastSongs";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 export async function GET() {
   const lastSongs = await getLastSongs();
