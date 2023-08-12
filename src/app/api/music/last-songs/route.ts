936be4e2ed53267510a1d63256cdc4779c354faa
Hamsterzs <abel090713@gmail.com>
2023-08-11 20:51:16 -0500

    Why is this even cached???

--- src/app/api/music/last-songs/route.ts
+++ src/app/api/music/last-songs/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import getLastSongs from "../../../../utils/getLastSongs";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const lastSongs = await getLastSongs();
 
